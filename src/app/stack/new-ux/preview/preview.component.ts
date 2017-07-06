a28b918d24d034fad169c5d0f07ffdb4f2ef9860
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-07-06 14:17:05 +0530

    More changes

--- src/app/stack/new-ux/preview/preview.component.ts
+++ src/app/stack/new-ux/preview/preview.component.ts
@@ -16,26 +16,43 @@ export class PreviewComponent {
         if (this.previewData) {
             console.log('Preview');
             console.log(this.previewData);
-
-            this.gaugeChart = {
-                data: {
-                    columns: [
-                        ['data', this.previewData.osio_user_count / 10]
-                    ],
-                    type: 'gauge'
-                },
-                options: {
-                    color: {
-                        pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'],
-                        threshold: {
-                            values: [30, 60, 90, 100]
+            if (this.previewData.hasOwnProperty('sentiment')) {
+                this.gaugeChart = {
+                    data: {
+                        columns: [
+                            ['data', this.previewData.sentiment.overall_score]
+                        ],
+                        type: 'gauge'
+                    },
+                    configs: {
+                        legend: {
+                            show: true
                         }
                     },
-                    size: {
-                        height: 180
+                    options: {
+                        gauge: {
+                            label: {
+                                format: function(value, ratio) {
+                                    return value;
+                                }
+                            },
+                            min: -1,
+                            max: 1,
+                            width: 39, // for adjusting arc thickness
+                            title: 'Sentiment Score'
+                        },
+                        color: {
+                            pattern: ['#cf2a0e', '#bdcf0e', '#368a55'],
+                            threshold: {
+                                values: [-.2, .2, 1]
+                            }
+                        },
+                        size: {
+                            height: 180
+                        }
                     }
-                }
-            };
+                };
+            }
 
         }
     }
