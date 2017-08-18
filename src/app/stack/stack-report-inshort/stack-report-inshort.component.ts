6101a597b5baac01362f42a34e264e720bcc34ed
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-08-18 16:58:29 +0530

    Initial version of new npm

--- src/app/stack/stack-report-inshort/stack-report-inshort.component.ts
+++ src/app/stack/stack-report-inshort/stack-report-inshort.component.ts
@@ -1,8 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, OnChanges, ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
     selector: 'stack-report-inshort',
     templateUrl: './stack-report-inshort.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    styleUrls: ['./stack-report-inshort.component.scss'],
 })
 
-export class StackReportInShortComponent {}
+export class StackReportInShortComponent implements OnInit, OnChanges {
+    public tabs: Array<any> = [];
+    constructor() {
+
+    }
+    ngOnInit(): void {
+        this.ngOnChanges();
+    }
+    ngOnChanges(): void {
+        this.tabs.push({
+            title: 'File 1',
+            content: 'Sample',
+            index: 0
+        });
+    }
+}
--- src/app/stack/stack-report-inshort/stack-report-inshort.module.ts
+++ src/app/stack/stack-report-inshort/stack-report-inshort.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
+import {TabsModule} from 'ng2-bootstrap';
 import {StackReportInShortComponent} from './stack-report-inshort.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        TabsModule.forRoot()
+    ],
     declarations: [StackReportInShortComponent],
     exports: [StackReportInShortComponent]
 })
